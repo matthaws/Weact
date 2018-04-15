7fb91687e04e572543fc0e295191ae1ec89e4604
Matt Haws <matthaws@gmail.com>
2018-04-15 12:44:02 -0400

    Remove async

--- src/demo/util/gif_api_util.js
+++ src/demo/util/gif_api_util.js
@@ -1,7 +1,7 @@
 import { APIKey } from "./secrets.js";
 
-export const fetchRandomGif = async (tag) => {
-  const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${APIKey}&tag=${tag}&rating=G`)
-  const gifInfo = await response.json();
-  return gifInfo;
-}
+// export const fetchRandomGif = async (tag) => {
+//   const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${APIKey}&tag=${tag}&rating=G`)
+//   const gifInfo = await response.json();
+//   return gifInfo;
+// }
